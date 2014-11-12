0d00d25d8dc8cb3dfbd79a3838855b15ab5e56ec
HongyuLi <hongyu@hongyutekiMacBook-Air.local>
2014-11-12 17:43:37 +0100

    slight change

--- HPC_Hough/main.c
+++ HPC_Hough/main.c
@@ -60,8 +60,8 @@ int main(int argc, const char * argv[]) {
             }
             //imSobelOut->img[j*h+i] =sobelFiltre(a1, a2, a3, b1, b2, b3, c1, c2, c3, SX, SY);
         }
-    }*/
-    write_pgm("test.bmp", im);
+    }
+    write_pgm("test.bmp", im);*/
 
     
 
